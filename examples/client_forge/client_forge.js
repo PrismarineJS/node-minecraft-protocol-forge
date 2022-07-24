ac30c13d8a1dd73510dd283c28cd2771ae55a353
Rob9315 <dev.robk@gmail.com>
2022-07-25 00:35:51 +0200

    formatting

--- examples/client_forge/client_forge.js
+++ examples/client_forge/client_forge.js
@@ -1,14 +1,14 @@
 var mc = require('minecraft-protocol');
 var autoVersionForge = require('../../src/client/autoVersionForge');
 
-if(process.argv.length < 4 || process.argv.length > 6) {
+if (process.argv.length < 4 || process.argv.length > 6) {
   console.log("Usage : node echo.js <host> <port> [<name>] [<password>]");
   process.exit(1);
 }
 
 var host = process.argv[2];
 var port = parseInt(process.argv[3]);
-var username =  process.argv[4] ? process.argv[4] : "echo";
+var username = process.argv[4] ? process.argv[4] : "echo";
 var password = process.argv[5];
 
 var client = mc.createClient({
@@ -32,19 +32,19 @@ const options = {
 autoVersionForge(client, options);
 
 // set up logging
-client.on('connect', function() {
+client.on('connect', function () {
   console.info('connected');
 });
-client.on('disconnect', function(packet) {
-  console.log('disconnected: '+ packet.reason);
+client.on('disconnect', function (packet) {
+  console.log('disconnected: ' + packet.reason);
 });
-client.on('end', function(err) {
+client.on('end', function (err) {
   console.log('Connection lost');
 });
-client.on('chat', function(packet) {
-  console.log('Received chat message:',packet);
+client.on('chat', function (packet) {
+  console.log('Received chat message:', packet);
 });
 
-client.on('forgeMods', function(mods) {
-  console.log('Received forgeMods event:',mods);
+client.on('forgeMods', function (mods) {
+  console.log('Received forgeMods event:', mods);
 });
