180b62956dcaa6181178ded414fa14c6b9ac821b
LetsChill <hahahamer2005@gmail.com>
2023-07-03 15:06:19 +0100

    Much more optimized.

--- src/client/forgeHandshake3.js
+++ src/client/forgeHandshake3.js
@@ -58,33 +58,8 @@ proto.addProtocol(require("./data/fml3.json"), ["fml3"])
 * }} options
 */
 
-module.exports = function (client, options) {
-
-  const mods = options.mods;
-  const channels = options.channels;
-  const registries = options.registries;
-
+module.exports = function (client) {
   client.tagHost = "\0FML3\0";
-  debug("Initialized FML2 handler")
-  if (!mods) {
-    debug("Trying to guess mods by reflecting the server")
-  } else {
-    debug("Mods:", mods)
-  }
-  if (!channels) {
-    debug("trying to guess channels by reflecting the servers'")
-  } else {
-    Object.entries(channels).forEach((name, marker) => {
-      debug("channel", name, marker)
-    })
-  }
-  if (!registries) {
-    debug("trying to guess registries by reflecting the servers'")
-  } else {
-    Object.entries(registries).forEach((name, marker) => {
-      debug("registry", name, marker)
-    })
-  }
 
   client.registerChannel("fml:loginwrapper", proto.types.fml_loginwrapper, false)
 
@@ -95,90 +70,31 @@ module.exports = function (client, options) {
 
   client.on("login_plugin_request", (data) => {
 
-    // 0: ModData
-    // 1: ModList
+    // 0: ModData // Ignore
+    // 1: ModList // Return Acknowledgement.
 
-    // 2 -> 20 Registry Data
-    // 21: Configuration
+    // 2 -> 20 Registry Data Return Acknowledgement.
+    // 21: Configuration Return Acknowledgement.
 
     if (data.channel === "fml:loginwrapper") {
-      // parse buffer
-      const { data: loginwrapper } = proto.parsePacketBuffer(
-        PROTODEF_TYPES.LOGINWRAPPER,
-        data.data
-      );
-
-      if (!loginwrapper.channel) {
-        console.error(loginwrapper);
-      }
-
-      console.log(data.messageId)
-
-      if (!loginwrapper.channel) {
-        console.error(loginwrapper);
-      }
-
       switch (data.messageId) {
 
         // ModData
         case (0):
-          console.log("Mod data recrieved, No response.")
           break;
-
-        case (1):
-          console.log("Mod List recieved, Should reply with modListReply!")
-
-          const modlistreply = {
-            modNames: options.modNames,
-            channels: [],
-            registries: []
-          }
-
-          const modlistreplypacket = proto.createPacketBuffer(
-            PROTODEF_TYPES.HANDSHAKE,
-            {
-              discriminator: "ModListReply",
-              data: modlistreply,
-            }
-          );
-
-          // loginWrapperPacket = proto.createPacketBuffer(
-          //   PROTODEF_TYPES.LOGINWRAPPER,
-          //   {
-          //     channel: FML_CHANNELS.HANDSHAKE,
-          //     data: modlistreplypacket,
-          //   }
-          // );
-          // console.log(modlistreplypacket)
-
-
-          // // client.write("login_plugin_response", {
-          // //   messageId: data.messageId,
-          // //   data: loginWrapperPacket,
-          // // });
-          // break
+        case (1): // I am not sure why.
 
         case (21):
-          console.log("This is configuration, should reply with Acknowledgement!")
-
           let AcknowledgementPacket = proto.createPacketBuffer(PROTODEF_TYPES.HANDSHAKE, { discriminator: "Acknowledgement" });
-
           let loginWrapperPacket = proto.createPacketBuffer(PROTODEF_TYPES.LOGINWRAPPER, { channel: FML_CHANNELS.HANDSHAKE, data: AcknowledgementPacket, });
-
           client.write("login_plugin_response", { messageId: data.messageId, data: loginWrapperPacket });
-
           break
 
         default:
           if (data.messageId > 1 && data.messageId < 21) {
-            console.log("Registry data, should reply with Acknowledgement!")
-
             let AcknowledgementPacket = proto.createPacketBuffer(PROTODEF_TYPES.HANDSHAKE, { discriminator: "Acknowledgement" });
-
             let loginWrapperPacket = proto.createPacketBuffer(PROTODEF_TYPES.LOGINWRAPPER, { channel: FML_CHANNELS.HANDSHAKE, data: AcknowledgementPacket, });
-
             client.write("login_plugin_response", { messageId: data.messageId, data: loginWrapperPacket });
-
             break
           }
       }
