a2406dffaf8b000d05b67e63c714e8bbdf4f8286
Mykola1453 <nickbakaev@gmail.com>
2024-01-03 18:20:14 +0200

    Better handling of other loginwrapperchannel (FML2, FML3)

--- src/client/forgeHandshake3.js
+++ src/client/forgeHandshake3.js
@@ -278,23 +278,15 @@ module.exports = function (client, options) {
         }
 
         default:
-          console.log(
-              'other loginwrapperchannel',
-              loginwrapper.channel,
-              'received'
-          )
+          console.log('other loginwrapperchannel', loginwrapper.channel, 'received')
           try {
-            console.log(
-                'attempting to acknowledge',
-                loginwrapper.channel,
-                'loginwrapperchannel'
-            )
-            let AcknowledgementPacket = proto.createPacketBuffer(PROTODEF_TYPES.HANDSHAKE, { discriminator: "Acknowledgement" });
-            let loginWrapperPacket = proto.createPacketBuffer(PROTODEF_TYPES.LOGINWRAPPER, { channel: FML_CHANNELS.HANDSHAKE, data: AcknowledgementPacket, });
-            client.write("login_plugin_response", { messageId: data.messageId, data: loginWrapperPacket });
+            console.log('attempting to acknowledge', loginwrapper.channel, 'loginwrapperchannel')
+            const AcknowledgementPacket = proto.createPacketBuffer(PROTODEF_TYPES.HANDSHAKE, { discriminator: 'Acknowledgement' })
+            const loginWrapperPacket = proto.createPacketBuffer(PROTODEF_TYPES.LOGINWRAPPER, { channel: FML_CHANNELS.HANDSHAKE, data: AcknowledgementPacket })
+            client.write('login_plugin_response', { messageId: data.messageId, data: loginWrapperPacket })
             break
           } catch (error) {
-            console.error(error);
+            console.error(error)
           }
           break
       }
